22285713342e67d2fc764fa6e2a8d9e27b80fd65
AdolisPali <53820085+AdolisPali@users.noreply.github.com>
2024-12-07 14:46:10 +0200

    Update nvim-tree.lua

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -7,6 +7,6 @@ return {
   },
   config = function()
     require("nvim-tree").setup ()
-    vim.api.nvim_set_keymap('n', '<C-c>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
   end,
 }
